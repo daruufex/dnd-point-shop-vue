ad5d815d4f275325ce294c0635840a57204c0230
= <=>
2023-12-07 16:48:04 +0100

    leftover classNames from react

--- src/components/StatList.vue
+++ src/components/StatList.vue
@@ -5,13 +5,13 @@ const statNames = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wis
 </script>
 
 <template>
-  <table className="w-full text-md md:text-xl">
+  <table classe="w-full text-md md:text-xl">
     <thead>
       <tr class="border-b-2 border-b-emerald-700">
-        <th className="p-2 md:p-3 text-left">Attribute</th>
-        <th className="p-2 md:p-3 text-center">Ability score</th>
-        <th className="p-2 md:p-3 text-center">Ancestry bonus</th>
-        <th className="p-2 md:p-3 text-center">Final score</th>
+        <th class="p-2 md:p-3 text-left">Attribute</th>
+        <th class="p-2 md:p-3 text-center">Ability score</th>
+        <th class="p-2 md:p-3 text-center">Ancestry bonus</th>
+        <th class="p-2 md:p-3 text-center">Final score</th>
       </tr>
     </thead>
 
