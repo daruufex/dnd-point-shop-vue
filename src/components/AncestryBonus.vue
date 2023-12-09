3b2f9b7cb065718287be954736dbea9dd8e248d0
Fran <47521284+daruufex@users.noreply.github.com>
2023-12-09 02:07:19 +0100

    Update AncestryBonus.vue

--- src/components/AncestryBonus.vue
+++ src/components/AncestryBonus.vue
@@ -1,23 +1,24 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps } from 'vue'
 
-const props = defineProps(['value']);
+const props = defineProps(['value'])
 
-const handleInput = (newValue) => {
+const handleInput = (event) => {
+  const newValue = event.target.value
   if (isNaN(Number(newValue))) {
-    $emit('updateAncestryBonus', 0);
-    return;
+    $emit('updateAncestryBonus', 0)
+    return
   }
   if (newValue.length > 1) {
-    $emit('updateAncestryBonus', newValue.slice(0, 1));
-    return;
+    $emit('updateAncestryBonus', newValue.slice(0, 1))
+    return
   }
-  $emit('updateAncestryBonus', newValue);
-};
+  $emit('updateAncestryBonus', newValue)
+}
 
 const handleFocusOut = (event) => {
-  if (event.target.value === '') $emit('updateAncestryBonus', 0);
-};
+  if (event.target.value === '') $emit('updateAncestryBonus', 0)
+}
 </script>
 
 <template>
